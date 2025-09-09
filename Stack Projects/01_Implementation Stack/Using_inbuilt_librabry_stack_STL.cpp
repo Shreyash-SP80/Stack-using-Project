bac75042a5a80101f4800b27dcd8ac7055fba274
Shreyash <cyberninjax45@gmail.com>
2025-09-09 23:15:46 +0530

    Stack

--- Stack Projects/01_Implementation Stack/Using_inbuilt_librabry_stack_STL.cpp
+++ Stack Projects/01_Implementation Stack/Using_inbuilt_librabry_stack_STL.cpp
@@ -2,6 +2,60 @@
 #include<stack>
 using namespace std;
 
+// Stack implementation
+// class Stack {
+//     //properties
+//     public:
+//         int *arr;
+//         int top;
+//         int size;
+
+//     // behaviour
+//     Stack(int size) {
+//         this -> size = size;
+//         arr = new int[size];
+//         top = -1;
+//     }
+
+//     void push( int element) {
+//         if(size - top > 1) {
+//             top++;
+//             arr[top] = element;
+//         }
+//         else{
+//             cout << "Stack OverFlow" << endl;
+//         }
+//     }
+
+//     void pop() {
+//         if(top >=0 ) {
+//             top--;
+//         }
+//         else{
+//             cout << "Stack UnderFlow" << endl;
+//         }
+//     }
+
+//     int peek() {
+//         if(top >=0 )
+//             return arr[top];
+//         else
+//         {
+//             cout << "Stack is Empty" << endl;
+//             return -1;
+//         }
+//     }
+
+//     bool isEmpty() {
+//         if( top == -1) {
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+
+// };
 
 int  main() {
 
@@ -63,4 +117,5 @@ int  main() {
     // }
     return 0;
 }
+
 
